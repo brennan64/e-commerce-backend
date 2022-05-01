c3eb8fe99929efbe74af4ba277a35b02101f47fb
Brennan <brennanheley@gmail.com>
2022-05-01 11:52:38 -0500

    erased some comments

--- config/connection.js
+++ config/connection.js
@@ -13,17 +13,3 @@ module.exports = new Sequelize(
     logging: false,
   }
 );
-
-// TODO: Uncomment the code below before turning in the project
-// and delete these lines
-
-// module.exports = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//       logging: false,
-//     });
